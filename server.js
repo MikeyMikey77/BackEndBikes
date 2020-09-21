a1032e4a9d9fcbc27a1b785ce3d83362e81bd0b4
MikeyMikey77 <mirakfuion@gmail.com>
2020-09-22 00:12:37 +0300

    commit message

--- server.js
+++ server.js
@@ -16,17 +16,17 @@ const mapTimer = new Map();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     // do logging
     console.log('Something is happening.');
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-    res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+    res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
     next(); // make sure we go to the next routes and don't stop here
 });
 
 //app.use(express.static(__dirname + "/public"));
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.json({ message: "hooray! welcome to our api!" });
 })
 
@@ -36,25 +36,18 @@ mongoClient.connect(function (err, client) {
     app.locals.collection = client.db("BikesRentalShop").collection("bikes");
     app.locals.collectionUsers = client.db("BikesRentalShop").collection("users");
 
-    // app.locals.collectionUsers.insertOne({ name: "John Wick" }, function(err, result) {
-
-    //     if (err) {
-    //         console.log(err);
-    //     }
-    // })
-
     let bikes = [
-        { name: "ИЖ «Юпитер»", type: "классический", price: "70", rented: true},
+        { name: "ИЖ «Юпитер»", type: "классический", price: "70", rented: true },
         { name: "Honda CBR1000RR Fireblade", type: "спортивный", price: "75", rented: true },
         { name: "Kawasaki KXF", type: "кроссовый", price: "100" },
         { name: "Harley-Davidson Fat Boy", type: "крузер", price: "150" },
         { name: "Урал-Днепр", type: "Чоппер", price: "50" }]
 
-    // app.locals.collection.insertMany(bikes, function(err, result) {
-    //     if (err) {
-    //         console.log(err);
-    //     }
-    // })
+    app.locals.collection.insertMany(bikes, function (err, result) {
+        if (err) {
+            console.log(err);
+        }
+    })
     //  app.locals.collection.insertOne({ name: "test", type: "tes", price: "100.25", rented: true })
 
     app.listen(port, function () {
@@ -137,18 +130,18 @@ app.put("/api/bikes/rent", function (req, res) {
     //console.log(typeof req.body.isToLowerPrice);
 
     if (isToLowerPrice) {
-        let timerId = setTimeout( () => {
-        let newPrice;
-        let p = collection.findOne({ _id: id }, function(err, result) {
-            newPrice = +result.price / 2;
-            collection.findOneAndUpdate({ _id: id }, { $set: { price: newPrice } },
-                { returnOriginal: false }, function (err, result) {
-        
-                    if (err) return console.log(err);
-                    console.log(result);
-                })
-        })
-         }, /* timer expected */72000000//20 hours
+        let timerId = setTimeout(() => {
+            let newPrice;
+            let p = collection.findOne({ _id: id }, function (err, result) {
+                newPrice = +result.price / 2;
+                collection.findOneAndUpdate({ _id: id }, { $set: { price: newPrice } },
+                    { returnOriginal: false }, function (err, result) {
+
+                        if (err) return console.log(err);
+                        console.log(result);
+                    })
+            })
+        }, /* timer expected */72000000//20 hours
         )
         mapTimer.set(id, timerId);
     }
